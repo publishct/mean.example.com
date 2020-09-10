b8f23c204dbe28d4a0915cb2464014680f25d07e
Cynthia Trujillo <developercynthia@gmail.com>
2020-09-10 12:03:30 -0500

    added Ionic users to whitelist subs

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(require('express-session')({
     //secure: true,
     maxAge:3600000 //1 hour
   }
+
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -75,6 +76,19 @@ passport.deserializeUser(function(user, done){
   done(null, user);
 });
 
+//Set up CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 //Working with Session Data
 app.use(function(req,res,next){
   res.locals.session = req.session;
@@ -84,7 +98,7 @@ app.use(function(req,res,next){
 //Session-based access control
 app.use(function(req,res,next){
   //Uncomment the following line to allow access to everything.
-  //return next();
+  return next();
 
   //Allow any endpoint that is an exact match. The server does not
   //have access to the hash so /auth and /auth#xxx would bot be considered 
@@ -108,7 +122,8 @@ app.use(function(req,res,next){
   var subs = [
     '/public/',
     '/api/auth/',
-    '/articles'
+    '/articles',
+    '/ionicUsers'
   ];
 
   //The query string provides a partial URL match beginning
